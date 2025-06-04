3c3903c1c7e8ac809339981a70ac98225f53d378
SCrbnll <samuelcarbonell1203@gmail.com>
2025-06-04 17:17:07 +0200

    [ADD] Stats

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,9 @@
 import CVIcon from "../components/icons/CVIcon.astro";
 import GitHubIcon from "../components/icons/GitHubIcon.astro";
+import JobIcon from "../components/icons/JobIcon.astro";
+import LanguageIcon from "../components/icons/LanguageIcon.astro";
 import LinkedInIcon from "../components/icons/LinkedInIcon.astro";
+import SchoolIcon from "../components/icons/SchoolIcon.astro";
 
 export const SITE_NAME = "Samuel Carbonell";
 export const DEFAULT_LANG = "es";
@@ -26,4 +29,40 @@ export const SOCIALS = [
     url: "https://github.com/SCrbnll/SCrbnllDev/blob/main/public/Curriculum%20Vitae.pdf",
     icon: CVIcon,
   }
+];
+
+export const STATS = [
+  {
+    icon: JobIcon,
+    title: "< 1 year of experience",
+    align: "center",
+    items: [
+      { title: "Frontend Developer", value: "4 months" },
+      { title: "Coming soon", value: "..." },
+    ],
+  },
+  {
+    icon: SchoolIcon,
+    title: "Graduated",
+    align: "end",
+    items: [
+      {
+        title: "FP Superior en Desarrollo de Aplicaciones Web",
+        value: "2024-2025",
+      },
+      {
+        title: "FP Superior en Desarrollo de Aplicaciones Multiplataforma",
+        value: "2022-2024",
+      },
+    ],
+  },
+  {
+    icon: LanguageIcon,
+    title: "Idiomas",
+    align: "center",
+    items: [
+      { title: "Spanish", value: "Native", img: "/flags/es.svg" },
+      { title: "English", value: "B2", img: "/flags/en.svg" },
+    ],
+  },
 ];
