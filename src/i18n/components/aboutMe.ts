607de685342c13aae884167beb7e0c9cf77d0818
SCrbnll <samuelcarbonell1203@gmail.com>
2025-06-07 22:20:08 +0200

    [MOD][ Text AboutMe

--- src/i18n/components/aboutMe.ts
+++ src/i18n/components/aboutMe.ts
@@ -2,7 +2,7 @@ export const aboutTranslations = {
   es: {
     title: 'Junior Fullstack Developer',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt blanditiis repellendus dolorum sapiente provident? Distinctio obcaecati reiciendis asperiores rem ratione at voluptatum veritatis, totam explicabo minus temporibus quia ipsum aliquid!',
+      'Soy un desarrollador junior con muchas ganas de aprender siempre que puedo. Cuando me dan un problema, no paro hasta resolverlo, aunque eso signifique pasar horas sin dormir. Soy bastante ambicioso y me esfuerzo para conseguir lo que me propongo',
     portfolio: 'Ver mi trabajo',
     contact: 'Contactame',
     blog: 'Más allá',
@@ -10,7 +10,7 @@ export const aboutTranslations = {
   en: {
     title: 'Junior Fullstack Developer',
     description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt blanditiis repellendus dolorum sapiente provident? Distinctio obcaecati reiciendis asperiores rem ratione at voluptatum veritatis, totam explicabo minus temporibus quia ipsum aliquid!',
+      "I'm a junior developer eager to learn whenever I can. When I'm given a problem, I don't stop until I solve it, even if it means spending hours without sleeping. I'm quite ambitious and I work hard to achieve what I set out to do.",
     portfolio: 'View my work',
     contact: 'Get in Touch',
     blog: 'Beyond Code',
