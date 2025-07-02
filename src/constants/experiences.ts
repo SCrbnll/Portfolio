43af3a9d80450834c076d79ccb4499fa8ba7d6f2
Samuel Carbonell <120050196+SCrbnll@users.noreply.github.com>
2025-07-02 13:29:55 +0200

    Actualizar experiences.ts

--- src/constants/experiences.ts
+++ src/constants/experiences.ts
@@ -5,7 +5,7 @@ export const EXPERIENCES = {
             company: "Adsalsa",
             href: "https://adsalsa.com/",
             logo: "https://www.adsalsa.com/wp-content/uploads/2020/05/logo-300x64.png",
-            period: "MAYO 2025 - JUNIO 2025",
+            period: "MARZO 2025 - JUNIO 2025",
             description: "Practicas como desarrollador frontend",
             responsibilities: [
                 "Participé en el desarrollo de BackOffice con Angular, utilizando Boilerplates para asegurar consistencia y buenas prácticas.",
@@ -20,7 +20,7 @@ export const EXPERIENCES = {
             company: "Mercanza",
             href: "https://www.mercanza.es/",
             logo: "https://www.mercanza.es/xen_media/logo-mercanza-white.svg",
-            period: "MAYO 2024 - JULIO 2024",
+            period: "MARZO 2024 - JULIO 2024",
             description: "Practicas como desarrollador frontend",
             responsibilities: [
                 "Aprendizaje autodidacta de Angular durante el período de prácticas.",
@@ -34,7 +34,7 @@ export const EXPERIENCES = {
             company: "Adsalsa",
             href: "https://adsalsa.com/",
             logo: "https://www.adsalsa.com/wp-content/uploads/2020/05/logo-300x64.png",
-            period: "MAY 2025 - JUN 2025",
+            period: "MAR 2025 - JUN 2025",
             description: "Frontend Developer Intern",
             responsibilities: [
                 "Participated in the development of BackOffice using Angular, using corporate Boilerplates to ensure consistency and good practices.",
@@ -49,7 +49,7 @@ export const EXPERIENCES = {
             company: "Mercanza",
             href: "https://www.mercanza.es/",
             logo: "https://www.mercanza.es/xen_media/logo-mercanza-white.svg",
-            period: "MAY 2024 - JUL 2024",
+            period: "MAR 2024 - JUL 2024",
             description: "Frontend Developer Intern",
             responsibilities: [
                 "Self-taught learning of Angular during the internship period.",
