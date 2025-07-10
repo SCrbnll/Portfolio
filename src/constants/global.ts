abf46d544af42816b2cfeaa71c93ddfa3a193dc0
SCrbnll <samuelcarbonell1203@gmail.com>
2025-07-10 10:38:18 +0200

    [MOD] Added Telegram Contact

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -4,8 +4,8 @@ export const LINKEDIN_URL = "https://www.linkedin.com/in/samuu-crbnll/";
 export const GITHUB_URL = "https://github.com/SCrbnll";
 export const EMAIL = "samuelcarbonell1203@gmail.com";
 export const EMAIL_DISPLAY = "samuelcarbonell1203";
-export const WHATSAPP_URL = "https://api.whatsapp.com/send/?phone=34693649721&text&type=phone_number&app_absent=0";
-export const PHONE_NUMBER = "+34 693649721";
+export const TELEGRAM_URL = "https://t.me/scrbnll";
+export const TELEGRAM_USER = "scrbnll";
 export const CV_URL = "/CV-Programmer.pdf";
 
 export const LANGUAGES = {
